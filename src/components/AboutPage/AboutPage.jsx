8731414b4d64caea218ea5425c5aa12679c1ac3f
Alex Vang <VangAlexa25@gmail.com>
2024-06-02 23:16:13 -0500

    about finished

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -9,7 +9,24 @@ function AboutPage() {
   return (
     <div className="container">
       <div>
-        <p>This about page is for anyone to read!</p>
+        <h4>Which technologies did you use?</h4>
+        <p>
+          React, Redux, SQL, Node, Express, and GIMP. GIMP is a free image
+          manipulation program where you can edit images, draw free-form, and
+          transcode between different image file formats.
+        </p>
+
+        <h4>What was the toughest challenge you overcame?</h4>
+        <p>
+          The toughest challenge I overcame was implementing a stored date that
+          ensures three new random exercises are received every day.
+        </p>
+
+        <h4>What is the one next thing you are excited to tackle?</h4>
+        <p>
+          The next challenge I'm excited to tackle is implementing leaderboards
+          to see other users' progress!
+        </p>
       </div>
     </div>
   );
